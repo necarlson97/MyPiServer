db06ad0a1ecbf0159cd4eb368f32f46e9a3ca87c
necarlson97 <nilscarlson1997@gmail.com>
2019-03-31 21:54:32 -0400

    coloring

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+// Set the draw-able height / width
+// TODO could make this dynamic
 const width = window.innerWidth;
 const height = window.innerHeight;
 var two = new Two({ width, height});
+// All streak lines will be placed in this group
+var group = two.makeGroup();
 
+// Swappable function to determine the
+// generation of new streaks
+// TODO idealy would account for the hypotonuse
+// length, rather than just w & h
 function randStart() {
   return Math.random() - .5;
 }
 
+// A function for generating streaks at the
+// center of the view, to be stretched,
+// and move otwards
 const maxLineStart = 500;
-function newStreak(group) {
+function newStreak() {
   const w = randStart();
   const h = randStart();
   const line = two.makeLine(0, 0,
@@ -18,13 +29,16 @@ function newStreak(group) {
   const x = w * maxLineStart * .5;
   const y = h * maxLineStart * .5;
 
+  // Allows us to easily move / rotate the whole
   group.add(line);
 
   streak = {x, y, w, h, line};
   return streak;
 }
 
-function update(streaks, group) {
+// A single update step for the streaks,
+// involves 
+function update(streaks) {
   for (let i=0; i<streaks.length; i++) {
     streak = streaks[i];
     streak.line.scale += 0.05;
@@ -32,7 +46,20 @@ function update(streaks, group) {
     streak.y += streak.h * maxLineStart * 0.1;
     streak.line.translation.set(streak.x, streak.y);
 
-    if (streak.x > width || streak.y > height
+    const maxX = width / 2;
+    const maxY = height - (height/3);
+
+    // Just some fun coloring;
+    let r = Math.abs(streak.x) / maxX;
+    const g = 50;
+    let b = Math.abs(streak.y) / maxY;
+    r *= 150;
+    b *= 150;
+
+    streak.line.stroke = `rgb(${r}, ${g}, ${b})`;
+
+    if (streak.x > maxX || streak.x < -maxX
+        || streak.y > maxY || streak.y < -maxY
         || streak.line.scale > 10) {
       streak.line.remove();
       streaks[i] = newStreak(group);
@@ -40,27 +67,43 @@ function update(streaks, group) {
   }
 }
 
+// Initilize two.js, create first streaks, and
+// set update function
+const streakCount = 200;
 window.onload = function() {
     // Simply set draw-able frame to starting width / height
     // Create 'two.js' canvas
-    var elem = document.getElementById('draw');
-
+    let elem = document.getElementById('draw');
     two.appendTo(elem);
 
-    var centerX = 0;
-    var centerY = 0;
+    group.translation.set(width / 2, height / 3);
 
-    var group = two.makeGroup();
-    group.translation.set(width / 2, height / 2);
-
-    var streaks = []
-    for (let i=0; i<200; i++) {
+    let streaks = []
+    for (let i=0; i<streakCount; i++) {
       streaks.push(newStreak(group));
     }
 
     two.bind('update', function(frameCount) {
-
-      let lifeCycle = frameCount * .01;
       update(streaks, group)
     }).play();
+}
+
+// Allows the center of the effect
+// to 'sway' with the mouse, moving
+// up to 'maxDeviation' pixels away
+const maxDeviation = 100;
+function mouseMove(event) {
+  const baseX = width/2;
+  const baseY = height/3;
+
+  let x = event.clientX / width;
+  let y = event.clientY / height;
+
+  x *= maxDeviation;
+  y *= maxDeviation;
+
+  x -= maxDeviation / 2;
+  y -= maxDeviation / 2;
+
+  group.translation.set(baseX + x, baseY + y);
 }
